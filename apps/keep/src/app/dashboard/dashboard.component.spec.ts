d2b977bd775e0ffcddacc5e1f57cfbf4abded1b5
Alexander Menne <alexander.menne@amenne.de>
2020-03-09 13:26:13 +0100

    Fixed failing tests for dashboard. Added material imports to the tests.

--- apps/keep/src/app/dashboard/dashboard.component.spec.ts
+++ apps/keep/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,16 @@
+// Angular
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Component
 import { DashboardComponent } from './dashboard.component';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,18 +18,25 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [DashboardComponent],
+      imports: [
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule
+      ]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DashboardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create', async(() => {
     expect(component).toBeTruthy();
-  });
+  }));
 });
