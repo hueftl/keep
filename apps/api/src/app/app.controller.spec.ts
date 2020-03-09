847c6907ebd3f7b2d15152ade882977247d59fa0
Alexander Menne <alexander.menne@amenne.de>
2020-03-09 12:07:09 +0100

    Fixed failing API test.

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -3,8 +3,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { Colour, GW_COLOURS } from './mocks/mock_colours';
+
 describe('AppController', () => {
   let app: TestingModule;
+  const gwColours = GW_COLOURS;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
@@ -16,7 +19,7 @@ describe('AppController', () => {
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(appController.getGwColours()).toEqual(gwColours);
     });
   });
 });
