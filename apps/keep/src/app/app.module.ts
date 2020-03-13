f9e5e65795033643e8b62deedf7303b3e8fd75f1
Alexander Menne <alexander.menne@amenne.de>
2020-03-13 09:28:36 +0100

    Removed dashboard and cleaned app module.

--- apps/keep/src/app/app.module.ts
+++ apps/keep/src/app/app.module.ts
@@ -1,33 +1,18 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// App related
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
-// Angular Material
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+// Modules
+import { AuthenticationModule } from './authentication/authentication.module';
+
+// Components
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule
-  ],
+  declarations: [AppComponent],
+  imports: [AuthenticationModule, AppRoutingModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
