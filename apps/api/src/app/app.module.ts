cfaa9a0c5c14a1ca8d85553bbacf60d8ef15fee6
Alexander Menne <alexander.menne@amenne.de>
2020-07-28 23:22:55 +0200

    fix failing unit test due to empy style and markup files

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,12 @@
 // NestJS
 import { Module } from '@nestjs/common';
 
-// Database (MongoDB)
-import { MongooseModule } from '@nestjs/mongoose';
-
 // API Application
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [],
   controllers: [AppController],
   providers: [AppService]
 })
